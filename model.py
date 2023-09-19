Added class weighting, changed plot to save to a file, added file to gitignore, fixed f1, accuracy calculation

model.py
@@ -1,14 +1,24 @@
 from transformers import BertModel
 import torch
 from torch import nn
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.bert = BertModel.from_pretrained("bert-base-uncased")
-        self.linear_layer = nn.Linear(in_features=768, out_features=26)
 
-    def forward(self, ids, attention_mask):
-        bert_sequences = self.bert(ids, attention_mask=attention_mask)
-        pooled_output = bert_sequences.pooler_output
-        linear_output = self.linear_layer(pooled_output)
-        return linear_output
+class NERModel(nn.Module):
+    def __init__(self, num_labels):
+        super(NERModel, self).__init__()
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
+        self.dropout = nn.Dropout(0.1)
+        self.bilstm = nn.LSTM(input_size=768, hidden_size=100, num_layers=2, batch_first=True, bidirectional=True)
+        self.linear_layer = nn.Linear(200, num_labels)  # 100 * 2 (bidirectional)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids, attention_mask=attention_mask)
+        sequence_output = outputs.last_hidden_state
+        sequence_output = self.dropout(sequence_output)
+
+        # Pass the output through the BiLSTM layer
+        lstm_output, _ = self.bilstm(sequence_output)
+
+        # Apply the linear layer to get NER label logits
+        logits = self.linear_layer(lstm_output)
+
+        return logits